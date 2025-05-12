97d6fb743c8d126bd5eb00d8e43530731b66c5ac
darrigu <darrigu.pro@gmail.com>
2025-05-12 14:39:24 +0200

    feat: print executing commands when building

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+set -xe
 zig cc --target=wasm32-freestanding --no-standard-libraries -Wl,--import-symbols -Wl,--export=game_width -Wl,--export=game_height -Wl,--export=game_update -Wl,--export=game_render -Wl,--no-entry -o back.wasm back.c
